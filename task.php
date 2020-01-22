85babfb484ade9213bade813794c16c41d6d1d4c
PawanBagga27 <60170226+PawanBagga27@users.noreply.github.com>
2020-01-22 11:45:48 +0530

    Create task.php

---
+++ task.php
@@ -0,0 +1,30 @@
+function task_function($var)
+{
+for ($i=1; $i <= $var ; $i++) {
+	switch ($i) {
+			case $i%5==0&&$i%3==0:
+			echo "Linianos";
+			echo "<br>"; 
+			echo "<pre>"; 
+			break;
+			case $i%3==0:
+			echo "Linio";
+			echo "<br>"; 
+			echo "<pre>"; 
+			break;
+
+			case $i%5==0:
+			echo "IT";
+			echo "<br>"; 
+			echo "<pre>"; 
+			break;
+			
+			default:
+			echo $i; 
+			echo "<br>"; 
+			echo "<pre>"; 
+			break;
+	}
+	}	
+}
+task_function(100); 
